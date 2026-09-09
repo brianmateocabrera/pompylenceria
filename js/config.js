8a90f5999a91e85248afd6fe69d80933e879e949
Brian Cabrera <57054546+brianmateocabrera@users.noreply.github.com>
2026-09-09 00:55:27 -0300

    Crear config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+export const PRODUCTOS_URL = "data/productos.json";
+export const IMAGES_PATH = "images/";
+export const PLACEHOLDER_IMAGE = "images/no-image.webp";
+export const WHATSAPP_NUMBER = "543518189444";
